f750a02fec85ebe2ba67bbe900eb298d0fc67fc1
emre <emreozturanli1@gmail.com>
2022-08-01 12:40:52 +0300

    employee table pagination and sort

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,129 +1,268 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import { Button, InputAdornment, TextField, Typography } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { useContext } from 'react';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import { Button, InputAdornment, TableHead, TextField, Typography } from '@mui/material';
 import { DbContext } from '../context/DbContext';
+import SearchIcon from '@mui/icons-material/Search';
 import AddEmployee from './AddEmployee';
+import TableSortLabel from '@mui/material/TableSortLabel';
 
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 export default function EmployeeTable() {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const { employees, setUpdateInfo, setDialogOpen } = useContext(DbContext)
-    const [search, setSearch] = React.useState('')
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const { employees, setUpdateInfo, setDialogOpen } = React.useContext(DbContext)
+  const [search, setSearch] = React.useState('')
 
-    const columns = [
-        { id: 'name', label: 'Name', minWidth: 100 },
-        { id: 'email', label: 'Email', minWidth: 100 },
-        { id: 'address', label: 'Address', minWidth: 100 },
-        { id: 'company', label: 'Company', minWidth: 100 }
-    ];
+  /*SORT PART */
 
-    function createData(name, email, address, company, id) {
-        return { name, email, address, company, id };
+  const [orderDirection, setOrderDirection] = React.useState('asc')
+  const [valueToOrderBy, setValueToOrderBy] = React.useState('name')
+
+  const handleRequestSort = (event, property) => {
+    const isAscending = (valueToOrderBy === property && orderDirection === 'asc')
+    setValueToOrderBy(property)
+    setOrderDirection(isAscending ? 'desc' : 'asc')
+  }
+
+  const createSortHandler = (property) => (event) => {
+    handleRequestSort(event, property)
+  }
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1
     }
-
-    const rows = search ? employees?.filter((item)=> item.name.toLowerCase().includes(search.toLowerCase())) : employees?.map((e) => createData(e.name, e.email, e.address, e.company, e.id))
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-    const emptyRows =
-        rowsPerPage - Math.min(rowsPerPage, rows?.length - page * rowsPerPage);
-
-    const handleRowClick = (row) => {
-        setUpdateInfo({ id: row.id, name: row.name, email: row.email, address: row.address, company: row.company })
+    if (b[orderBy] > a[orderBy]) {
+      return 1
     }
+    return 0
+  }
 
-    return (
-        <Paper sx={{ padding: '1rem' }} elevation={24}>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Typography variant='h4' >Employees</Typography>
-                <TextField
-                    variant="outlined"
-                    sx={{marginLeft: '1rem'}}
-                    placeholder='Search for an employee'
-                    size='small'
-                    value={search}
-                    onChange={(e)=>setSearch(e.target.value)}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <SearchIcon />
-                            </InputAdornment>
-                        )
-                    }}
-                />
-            </div>
-            <TableContainer sx={{ maxHeight: 540 }}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns?.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{ minWidth: column.minWidth }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows
-                            ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            ?.map((row, i) => {
-                                return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={i} sx={{ cursor: 'pointer' }} onClick={() => handleRowClick(row)}>
+  const getComparator = (order, orderBy) => {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy)
+  }
 
-                                        {columns?.map((column, i) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell key={i} align={column.align}>
-                                                    {column.format && typeof value === 'number'
-                                                        ? column.format(value)
-                                                        : value}
-                                                </TableCell>
-                                            );
-                                        })}
+  const sortedRowInformation = (rowArray, comparator) => {
+    const stabilizedRowArray = rowArray.map((el, index) => [el, index])
+    stabilizedRowArray.sort((a, b) => {
+      const order = comparator(a[0], b[0])
+      if (order !== 0) return order;
+      return a[1] - b[1]
+    })
+    return stabilizedRowArray.map((el) => el[0])
+  }
 
-                                    </TableRow>
-                                );
-                            })}
-                        {emptyRows > 0 && (
-                            <TableRow style={{ height: 53 * emptyRows }}>
-                                <TableCell colSpan={6} />
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <Button onClick={() => setDialogOpen(true)}>Add New Employee</Button>
-                <TablePagination
-                    rowsPerPageOptions={[5, 10, 15]}
-                    component="div"
-                    count={rows?.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </div>
-            <AddEmployee />
-        </Paper>
-    );
+  const columns = [
+    { id: 'name', label: 'Name', minWidth: 100 },
+    { id: 'email', label: 'Email', minWidth: 100 },
+    { id: 'address', label: 'Address', minWidth: 100 },
+    { id: 'company', label: 'Company', minWidth: 100 }
+  ];
+
+  function createData(name, email, address, company, id) {
+    return { name, email, address, company, id };
+  }
+
+  const rowInfo = employees?.map((e) => createData(e.name, e.email, e.address, e.company, e.id))
+  const searchedRowInfo = employees?.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+
+  const rows = search ? sortedRowInformation(searchedRowInfo, getComparator(orderDirection, valueToOrderBy)) : sortedRowInformation(rowInfo, getComparator(orderDirection, valueToOrderBy))
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows?.length - page * rowsPerPage);
+
+  const handleRowClick = (row) => {
+    setUpdateInfo({ id: row.id, name: row.name, email: row.email, address: row.address, company: row.company })
+  }
+
+  return (
+    <Paper sx={{ padding: '1rem' }} elevation={24}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: { xs: 'center' },
+          flexDirection: { md: 'row', xs: 'column' }
+        }}
+        component='section'
+      >
+        <Typography variant='h4' >Employees</Typography>
+        <TextField
+          variant="outlined"
+          sx={{ marginLeft: '1rem' }}
+          placeholder='Search for an employee'
+          size='small'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+      </Box>
+      <TableContainer sx={{ maxHeight: 540 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns?.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  <TableSortLabel
+                    active={valueToOrderBy === column.id}
+                    direction={valueToOrderBy === column.id ? orderDirection : 'asc'}
+                    onClick={createSortHandler(column.id)}
+                  >
+                    {column.label}
+                  </TableSortLabel>
+
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              ?.map((row, i) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={i} sx={{ cursor: 'pointer' }} onClick={() => handleRowClick(row)}>
+
+                    {columns?.map((column, i) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={i} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+
+                  </TableRow>
+                );
+              })}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        // alignItems: { xs: 'center' },
+        flexDirection: { sm: 'row', xs: 'column' }
+      }} >
+        <Button onClick={() => setDialogOpen(true)} sx={{ order: {xs : 2, sm : 0}}}>Add New Employee</Button>
+
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          SelectProps={{
+            inputProps: {
+              'aria-label': 'rows per page',
+            },
+            native: true,
+          }}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          ActionsComponent={TablePaginationActions}
+        />
+      </Box>
+      <AddEmployee />
+    </Paper>
+
+  );
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,19 +1,17 @@
 import Grid from '@mui/material/Grid';
 import { useContext } from 'react';
 import EmployeeDetails from '../components/EmployeeDetails';
-import EmployeeTable from '../components/EmployeeTable';
 import { AuthContext } from '../context/AuthContext';
+import EmployeeTable from '../components/EmployeeTable'
 
 const Dashboard = () => {
-  const {user} = useContext(AuthContext)
-console.log(user)
+  const { user } = useContext(AuthContext)
+  console.log(user)
 
-
-
-    return (
+  return (
     <main>
-        {
-          user  && <Grid container sx={{ paddingInline: '2rem' }} spacing={4}>
+      {
+        user && <Grid container sx={{ paddingInline: '2rem' }} spacing={4}>
           <Grid item xs={12} lg={6} >
             <EmployeeTable sx={{ margin: 'auto' }} />
           </Grid>
@@ -21,11 +19,9 @@ console.log(user)
             <EmployeeDetails />
           </Grid>
         </Grid>
-        }
-        
-
+      }
     </main>
   )
-  }
-  
+}
+
 export default Dashboard
