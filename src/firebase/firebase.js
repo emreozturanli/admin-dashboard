99a4ac9dd3a2d2b09b8d6a9c46cba4ed35c44619
emre <emreozturanli1@gmail.com>
2022-08-03 11:10:42 +0300

    firebase config edit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth} from "firebase/auth";
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuvzmNbpoBMm1bMNVLAGrjQiGoc6FS2MY",
-  authDomain: "admin-dashboard-2a83c.firebaseapp.com",
-  databaseURL: "https://admin-dashboard-2a83c-default-rtdb.firebaseio.com",
-  projectId: "admin-dashboard-2a83c",
-  storageBucket: "admin-dashboard-2a83c.appspot.com",
-  messagingSenderId: "683246985083",
-  appId: "1:683246985083:web:f5c21b6a90eb4cc81c67de"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
