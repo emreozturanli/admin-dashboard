a21e298e806b81df944fb46aca4b432ca2f2093f
emre <emreozturanli1@gmail.com>
2022-08-10 14:15:32 +0300

    minor edits

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 * I used Firebase Realtime Database feature to store the data and Firebase Authentication feature for authentication operations.
 * I used React Router Dom for page routing.
 * Only Admin can login and add,update and delete employee datas.
+* Sing in datas are written as placeholders. You can use them to sign in.
+* PLEASE only edit or delete the datas that you add.
+
+### Tools
+
+* React
+* Material UI
+* Apex Charts
+* Firebase
+* React Router
+* React Icons
+* Context API
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,8 +10,8 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {AuthContext} from '../context/AuthContext'
-import {Navigate, useNavigate} from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 function Copyright(props) {
   return (
@@ -29,81 +29,81 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Login() {
-  const {email, setEmail,password,setPassword,login,user} = React.useContext(AuthContext)
+  const { email, setEmail, password, setPassword, login, user } = React.useContext(AuthContext)
   const navigate = useNavigate()
 
 
   return (
-    !user 
-    ? <ThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={(e) => login(e, navigate)} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            value={email}
-            onChange={(e)=>setEmail(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e)=>setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+    !user
+      ? <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
           >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2" onClick={()=>navigate('/forgot-password')}>
-                Forgot password?
-              </Link>
-            </Grid>
-            {/* <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid> */}
-          </Grid>
-        </Box>
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
-    </Container>
-  </ThemeProvider>
-  : 
-  <Navigate to='/dashboard'/> 
-    
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" onSubmit={(e) => login(e, navigate)} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                placeholder='admin@admin.xyz'
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                placeholder='123456'
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2" onClick={() => navigate('/forgot-password')}>
+                    Forgot password?
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Typography variant='body2' sx={{ mt: 6 }}>
+            *Use placeholders to sign in.
+          </Typography>
+          <Copyright sx={{ mt: 2, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
+      :
+      <Navigate to='/dashboard' />
+
   );
 }
